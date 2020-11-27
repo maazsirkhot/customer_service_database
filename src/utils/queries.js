35a86b0712e5ac44cdee2f2249276de1b4c6ae94
kamdarkarnavee <kamdarkarnavee@gmail.com>
2020-11-26 21:19:43 -0800

    Update queries.js

--- src/utils/queries.js
+++ src/utils/queries.js
@@ -31,8 +31,7 @@ module.exports = {
   VIEWS: {
     VIEW_ISSUE_WITH_COMMENTS: 'SELECT * FROM v_issue_with_comments WHERE issue_id = ?;',
     VIEW_EMPLOYEES_IN_PROJECT: '',
-    VIEW_UNASSIGNED_ISSUES: '',
-    VIEW_ISSUES_FOR_PROJECT: '',
+    VIEW_ISSUES_FOR_PROJECT: 'SELECT * FROM v_employee_issues_details WHERE project_id = ?;',
   },
   CUSTOMER_VIEWS: {
     VIEW_ISSUES_CREATED_BY_CUSTOMER: 'SELECT issue_id, issue_description as description,  issue_status as status, issue_resolved_date as resolved_date, issue_created_on as created_on, issue_last_modified_on as last_modified_on, project_name FROM v_customer_issues_details WHERE customer_id = ?;',
@@ -42,9 +41,11 @@ module.exports = {
   EMPLOYEE_VIEWS: {
     VIEW_PROJECTS_IN_DEPARTMENT: 'select * from v_projects_in_department WHERE dept_name = ? OR dept_id = ?;',
     VIEW_ISSUES_CREATED_BY_CUSTOMER: 'SELECT * FROM v_customer_issues_details WHERE customer_id = ?;',
-    VIEW_RESOLVED_ISSUES_BY_PROJECT: 'SELECT * FROM v_customer_issues_details WHERE issue_resolved_date IS NOT NULL AND issue_resolved_date <= DATE(NOW()) AND (project_id = ? OR project_name = ?);',
-    VIEW_ONGOING_ISSUES_BY_PROJECT: 'SELECT * FROM v_customer_issues_details WHERE issue_resolved_date IS NULL AND (project_id = ? OR project_name = ?);',
-    VIEW_PROJECTS_FOR_EMPLOYEE: '',
+    VIEW_RESOLVED_ISSUES_BY_PROJECT: 'SELECT * FROM v_customer_issues_details WHERE issue_resolved_date IS NOT NULL AND issue_resolved_date <= DATE(NOW()) AND project_id = ?;',
+    VIEW_ONGOING_ISSUES_BY_PROJECT: 'SELECT * FROM v_customer_issues_details WHERE issue_resolved_date IS NULL AND project_id = ?;',
+    VIEW_ONGOING_ISSUES_BY_PROJECT: 'SELECT * FROM v_customer_issues_details WHERE project_id = ?;',
+    VIEW_PROJECTS_OF_EMPLOYEE: 'SELECT DISTINCT project_id, project_name, dept_id FROM v_employee_issues_details WHERE assignee_id = ?;',
+    VIEW_UNASSIGNED_ISSUES: 'SELECT issue_id, description, status, resolved_date, created_on, last_modified_on, project_id, project_name, dept_id FROM v_employee_issues_details WHERE assignee_id IS NULL;',
   },
   CHECKS: {
     IS_EMPLOYEE_MANAGER: 'SELECT * FROM department WHERE manager_id = ?;',
